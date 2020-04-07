99b079376ab6ad2517faf900deecca7d69d8bc3e
karolramos <karollamendonca@gmail.com>
2020-04-06 21:51:51 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,13 +1,10 @@
-export default function initFetchBitcoin() {
-  fetch('https://www.blockchain.com/ticker')/*  API - site onde vai puxar os valores em bitcoin - a partir dessa resposta vai pro then  */
+export default function fetchBitcoin(url, target) {
+  fetch(url)/*  API - site onde vai puxar os valores em bitcoin - a partir dessa resposta vai pro then  */
     .then(response => response.json()) /* then   pegou a resposta e vai transformar => em .json */
     .then((bitcoin) => { /*  o .JSON  -  agora vai usar o arq abaixo */
-      const btcPreco = document.querySelector('.btc-preco'); /* selecionando o Li q ta com o doe bitcoin  */
+      const btcPreco = document.querySelector(target); /* selecionando o Li q ta com o doe bitcoin  */
       btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4); /* Colocando dentro de LI o valor em bitcoin - valor de venda em real. Tofixed transformou em 4 casas  */
-
-    }).catch(erro => { /* pegando erro  */
-      console.log(Error(erro));
-    })
+    }).catch(erro => console.log(Error(erro)));
 }
 
 //  https://www.blockchain.com/ticker
